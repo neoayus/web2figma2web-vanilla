130bb9c0f6f703206f5dca64bd8e59b8d49e374e
neoayus <ayus43@outlook.com>
2025-07-31 13:33:05 +0530

    feat: hamburger menu functionality

--- app.js
+++ app.js
@@ -7,3 +7,48 @@ sb.forEach((button)=>{
         button.classList.add("primary");
     })
 });
+
+// Hamburger button CTA: 
+let ham = document.querySelector(".icon.hamburger-menu");
+ham.addEventListener('touchstart',()=>{
+    // icons to hide
+    let apple = document.querySelector(".icon.apple");
+    let search = document.querySelector(".icon.search");
+    let store = document.querySelector(".icon.store");
+
+    let iconsToHide = [apple, search, store, ham];
+    iconsToHide.forEach((icon)=>{
+        icon.classList.add("hidden");
+    });
+
+    // icon to unhine : cross icon
+    let cross = document.querySelector(".icon.cross");
+    cross.classList.remove("hidden");
+    
+    // nav dropdown list 
+    let list = document.querySelector(".nav-list");
+    list.classList.remove("hidden");
+});
+
+// Cross Button CTA: 
+let cross = document.querySelector(".icon.cross");
+cross.addEventListener('touchstart',()=>{
+    // hide list 
+    let list = document.querySelector(".nav-list");
+    list.classList.add("hidden");
+    
+    // hide cross icon 
+    let cross = document.querySelector(".icon.cross");
+    cross.classList.add("hidden");
+
+    // un-hide multiple icons 
+    let apple = document.querySelector(".icon.apple");
+    let search = document.querySelector(".icon.search");
+    let store = document.querySelector(".icon.store");
+    let ham = document.querySelector(".icon.hamburger-menu");
+
+    let iconsToUnHide = [apple, search, store, ham];
+    iconsToUnHide.forEach((icon)=>{
+        icon.classList.remove("hidden");
+    });
+});
